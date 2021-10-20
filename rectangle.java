9a3fbab1d53fabd58a4c705ff1a3f82bf7e0230a
Ashish <ashishgour567@gmail.com>
2021-10-20 11:15:14 +0530

    java file

---
+++ rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle
+{
+    public static void main(String []args)
+    {
+        int l=6;
+        int h=4;
+        int area= l*h;
+        System.out.println("Area of rectangle is "+area);
+    }
+}
